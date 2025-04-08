f06e266bc43cb20f350a1b2aa10c68b00e1eb94b
Alberto Mitroi <alberto.mitroi@encora.com>
2025-04-08 10:58:43 +0300

    change program.cs

--- MAVI.API/Program.cs
+++ MAVI.API/Program.cs
@@ -14,15 +14,12 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 {
-    if (app.Environment.IsDevelopment()) 
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
     {
-        app.UseSwagger();
-        app.UseSwaggerUI(c =>
-        {
-            c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-        });
-    }
-
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    });
+    
     app.UseHttpsRedirection();
     app.MapControllers();
     app.Run();
