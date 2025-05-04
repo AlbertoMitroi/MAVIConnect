0b3d911d2fb8a29082da176401b55f5ce2b35525
NitaMario <marionita2664@gmail.com>
2025-05-05 02:02:09 +0300

    add api to angular service

--- MAVI.API/Program.cs
+++ MAVI.API/Program.cs
@@ -15,6 +15,16 @@ var builder = WebApplication.CreateBuilder(args);
     });
 
     builder.Services.AddDbContext<DataDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+    builder.Services.AddCors(options =>
+    {
+        options.AddPolicy("AllowAngularApp", policy =>
+        {
+            policy.WithOrigins("http://localhost:4200", "https://brave-sea-0d03a1c03.6.azurestaticapps.net/")
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+    });
 }
 
 var app = builder.Build();
@@ -25,7 +35,8 @@ var app = builder.Build();
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
     });
-    
+
+    app.UseCors("AllowAngularApp");
     app.UseHttpsRedirection();
     app.MapControllers();
     app.Run();
