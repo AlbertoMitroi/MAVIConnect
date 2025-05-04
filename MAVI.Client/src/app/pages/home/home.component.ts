537726834a886267bbf933ea465bd6771af11aab
AlbertoMitroi <73647184+AlbertoMitroi@users.noreply.github.com>
2025-05-04 14:53:00 +0300

    home page

--- MAVI.Client/src/app/pages/home/home.component.ts
+++ MAVI.Client/src/app/pages/home/home.component.ts
@@ -1,10 +1,5 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "../../../components/header/header.component";
-import { FooterComponent } from "../../../components/footer/footer.component";
-import { SidebarComponent } from "../../components/sidebar/sidebar.component";
 import { NavbarComponent } from "../../components/navbar/navbar.component";
-import { SuggestedFriendsComponent } from "../../components/suggested-friends/suggested-friends.component";
-import { TrendsForYouComponent } from "../../components/trends-for-you/trends-for-you.component";
 import { MainComponent } from "../../components/main/main.component";
 
 @Component({
